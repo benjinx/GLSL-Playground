e179dfaf5969a6290df3d16ef07b517fbdec5ca8
Benjinx <mynameizkevin@gmail.com>
2019-08-01 17:53:04 -0400

    Revert, we're gunna just use the same example for this shader example just an additional class instead of having it separate.

--- include/Chapter2Shader/Chapter2Shader.hpp
+++
@@ -1,9 +0,0 @@
-#ifndef CHAPTER_2_SHADER_HPP
-#define CHAPTER_2_SHADER_HPP
-
-class Chapter2Shader {
-public:
-    void Start();
-};
-
-#endif // CHAPTER_2_SHADER_HPP
--- src/main.cpp
+++ src/main.cpp
@@ -1,15 +1,10 @@
 #include <Chapters1and2/Chapters1and2.hpp>
-#include <Chapter2Shader/Chapter2Shader.hpp>
 
 int main()
 {
     // Examples from the first 2 chapters
-    //Chapters1and2 example1;
-    //example1.Start();
-
-    // Separate Shader example from chapter 2
-    Chapter2Shader example2;
-    example2.Start();
+    Chapters1and2 example1;
+    example1.Start();
 
     return 0;
 }
