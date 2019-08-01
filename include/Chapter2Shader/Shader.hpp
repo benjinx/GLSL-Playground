6884aab6f1cb5d766dcc8eb7aae46898f9a2e3c1
Benjinx <mynameizkevin@gmail.com>
2019-08-01 17:07:48 -0400

    Start of Chapter 2s ending shader example.

---
+++ include/Chapter2Shader/Shader.hpp
@@ -0,0 +1,52 @@
+#ifndef SHADER_HPP
+#define SHADER_HPP
+
+#include <string>
+
+#include <OpenGL.hpp>
+
+class Shader {
+public:
+    Shader();
+    ~Shader();
+
+    void Load(std::string vertFile, std::string fragFile);
+
+    std::string LoadShaderAsString(std::string fileName);
+    void PrintVersions();
+    void PrintExtensions();
+
+    void SaveShaderProgramAsBinary();
+    void LoadShaderProgramAsBinary();
+    void LoadShaderProgramAsSPRIV();
+
+    void SendTriangleData();
+    void GetActiveVertexInputAttribs();
+    void GetActiveUniformVariables();
+
+    void SendBlobData();
+
+    void CreateShaderProgramViaPipeline1();
+    void CreateShaderProgramViaPipeline2();
+    GLuint CompileShaderForPipeline(const std::string& fileName, GLenum shaderType);
+    void CheckLinkStatus(GLuint program);
+
+    void CreatePipeLines();
+    void CreateVAO();
+
+    void Render();
+    void RenderPipelines(int width, int height);
+
+    // Helper functions
+    const char* GetTypeString(GLenum type);
+
+private:
+    GLuint _mVaoHandle;
+    GLuint _mProgramHandle;
+
+    // Pipeline Vars
+    GLuint _mPrograms[3];
+    GLuint _mPipelines[2];
+};
+
+#endif // SHADER_HPP
