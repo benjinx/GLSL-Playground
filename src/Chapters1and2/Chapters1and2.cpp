512171f9545f19fa8be41379705e1547f775ac75
Benjinx <mynameizkevin@gmail.com>
2019-08-01 15:52:17 -0400

    Debug callback/messaging example now working.

--- src/Chapters1and2/Chapters1and2.cpp
+++ src/Chapters1and2/Chapters1and2.cpp
@@ -19,10 +19,57 @@ void processInput(GLFWwindow* window)
     }
 }
 
+std::string SourceToString(GLenum source)
+{
+    switch (source)
+    {
+    case GL_DEBUG_SOURCE_API:               return "GL_DEBUG_SOURCE_API";
+    case GL_DEBUG_SOURCE_WINDOW_SYSTEM:     return "GL_DEBUG_SOURCE_WINDOW_SYSTEM";
+    case GL_DEBUG_SOURCE_SHADER_COMPILER:   return "GL_DEBUG_SOURCE_SHADER_COMPILER";
+    case GL_DEBUG_SOURCE_THIRD_PARTY:       return "GL_DEBUG_SOURCE_THIRD_PARTY";
+    case GL_DEBUG_SOURCE_APPLICATION:       return "GL_DEBUG_SOURCE_APPLICATION";
+    case GL_DEBUG_SOURCE_OTHER:             return "GL_DEBUG_SOURCE_OTHER";
+    default:                                return NULL;
+    }
+}
+
+std::string TypeToString(GLenum type)
+{
+    switch (type)
+    {
+    case GL_DEBUG_TYPE_ERROR:               return "GL_DEBUG_TYPE_ERROR";
+    case GL_DEBUG_TYPE_DEPRECATED_BEHAVIOR: return "GL_DEBUG_TYPE_DEPRECATED_BEHAVIOR";
+    case GL_DEBUG_TYPE_UNDEFINED_BEHAVIOR:  return "GL_DEBUG_TYPE_UNDEFINED_BEHAVIOR";
+    case GL_DEBUG_TYPE_PORTABILITY:         return "GL_DEBUG_TYPE_PORTABILITY";
+    case GL_DEBUG_TYPE_PERFORMANCE:         return "GL_DEBUG_TYPE_PERFORMANCE";
+    case GL_DEBUG_TYPE_MARKER:              return "GL_DEBUG_TYPE_MARKER";
+    case GL_DEBUG_TYPE_PUSH_GROUP:          return "GL_DEBUG_TYPE_PUSH_GROUP";
+    case GL_DEBUG_TYPE_POP_GROUP:           return "GL_DEBUG_TYPE_POP_GROUP";
+    case GL_DEBUG_TYPE_OTHER:               return "GL_DEBUG_TYPE_OTHER";
+    default:                                return NULL;
+    }
+}
+
+std::string SeverityToString(GLenum severity)
+{
+    switch (severity)
+    {
+    case GL_DEBUG_SEVERITY_HIGH:            return "GL_DEBUG_SEVERITY_HIGH";
+    case GL_DEBUG_SEVERITY_MEDIUM:          return "GL_DEBUG_SEVERITY_MEDIUM";
+    case GL_DEBUG_SEVERITY_LOW:             return "GL_DEBUG_SEVERITY_LOW";
+    case GL_DEBUG_SEVERITY_NOTIFICATION:    return "GL_DEBUG_SEVERITY_NOTIFICATION";
+    default:                                return NULL;
+    }
+}
+
 void APIENTRY debugCallback(GLenum source, GLenum type, GLuint id, GLenum severity, GLsizei length, const GLchar* message, const void* param)
 {
-    // Convert GLenum parameters to strings 
-    printf("%s:%s[%s](%d): %s\n", glGetString(source), glGetString(type), glGetString(severity), id, message);
+    // Convert GLenum parameters to strings
+    std::string SourceString = SourceToString(source);
+    std::string TypeString = TypeToString(type);
+    std::string SeverityString = SeverityToString(severity);
+
+    printf("%s:%s[%s](%d): %s\n", SourceString.c_str(), TypeString.c_str(), SeverityString.c_str(), id, message);
 }
 
 
@@ -39,7 +86,7 @@ void Chapters1and2::Start()
     Shader* shader = new Shader();
 
     glDebugMessageCallback(debugCallback, nullptr);
-    glDebugMessageControl(GL_DONT_CARE, GL_DONT_CARE, GL_DONT_CARE, 0, NULL, GL_TRUE);
+    //glDebugMessageControl(GL_DONT_CARE, GL_DONT_CARE, GL_DONT_CARE, 0, NULL, GL_TRUE);
 
     shader->PrintVersions();
     //shader->PrintExtensions();
